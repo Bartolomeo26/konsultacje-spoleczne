7759fecf76ceb2f2a8f2c0a8ee93b9a4e0016fe2
Bartolomeo <barca260711@gmail.com>
2024-10-26 14:26:14 +0200

    add responsiveness to nav and footer

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,35 +1,49 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import classes from '../styles/MainNavigation.module.css';
-import logo from '../assets/logo.png'
+import logo from '../assets/logo.png';
+
 function MainNavigation()
 {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
-        <>
-            <header className="bg-cyan-800">
-                <Link to='/'><img className={classes.logo} src={logo} alt="logo" /></Link>
-                <nav>
-                    <ul className={classes.nav__links}>
-                        <li>
-                            <Link to='/'>Home</Link>
-                        </li>
-                        <li>
-                            <Link to='/communities'>Communities</Link>
-                        </li>
-                        <li>
-                            <Link to='/communities/new'>Create a Community</Link>
-                        </li>
-                        <li>
-                            <Link to='/faq'>FAQ</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <div>
+        <header className="bg-cyan-800 p-4 px-10 flex justify-between items-center">
+            <Link to='/'><img className={classes.logo} src={logo} alt="logo" /></Link>
 
-                    <Link to="/signup"><button type="button" className={classes.button}>Sign In</button></Link>
-                </div>
-            </header>
+            {/* Mobile Menu Button */}
+            <button
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                className="md:hidden text-white p-2"
+                aria-label="Toggle Menu"
+            >
+                ☰
+            </button>
 
-        </>);
+            <nav className={`${isMenuOpen ? "block" : "hidden"} md:flex`}>
+                <ul className={`md:flex space-y-4 md:space-y-0 md:space-x-8 ${classes.nav__links}`}>
+                    <li>
+                        <Link to='/' onClick={() => setIsMenuOpen(false)}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/communities' onClick={() => setIsMenuOpen(false)}>Communities</Link>
+                    </li>
+                    <li>
+                        <Link to='/communities/new' onClick={() => setIsMenuOpen(false)}>Create a Community</Link>
+                    </li>
+                    <li>
+                        <Link to='/faq' onClick={() => setIsMenuOpen(false)}>FAQ</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <div className="hidden md:block">
+                <Link to="/signup">
+                    <button type="button" className={classes.button}>Sign In</button>
+                </Link>
+            </div>
+        </header>
+    );
 }
 
 export default MainNavigation;
